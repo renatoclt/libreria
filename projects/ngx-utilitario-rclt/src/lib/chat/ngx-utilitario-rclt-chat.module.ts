01b21af74674a04adaa780b47f415cc61679786b
jovad06@gamil.com <masyyo12345>
2019-05-16 17:21:17 -0500

    creacion de componente chat-conversacion

--- projects/ngx-utilitario-rclt/src/lib/chat/ngx-utilitario-rclt-chat.module.ts
+++ projects/ngx-utilitario-rclt/src/lib/chat/ngx-utilitario-rclt-chat.module.ts
@@ -4,9 +4,10 @@ import { ChatComponent } from './chat/chat.component';
 import { ChatBusquedaComponent } from './chat/chat-busqueda/chat-busqueda.component';
 import { ChatListaComponent } from './chat/chat-lista/chat-lista.component';
 import { ChatDetalleComponent } from './chat/chat-detalle/chat-detalle.component';
+import { ChatConversacionComponent } from './chat/chat-conversacion/chat-conversacion.component';
 
 @NgModule({
-  declarations: [ChatComponent, ChatBusquedaComponent, ChatListaComponent, ChatDetalleComponent ],
+  declarations: [ChatComponent, ChatBusquedaComponent, ChatListaComponent, ChatDetalleComponent, ChatConversacionComponent ],
   imports: [
     CommonModule
   ],
