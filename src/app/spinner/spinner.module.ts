8fd1c5ea7d5db316f3d7901768616595372a1757
renato Cohaila <renatoclt@gmail.com>
2019-10-09 22:48:43 -0500

    pinner

--- src/app/spinner/spinner.module.ts
+++ src/app/spinner/spinner.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { SPINNER_INDICATOR_CONFIG, DEFAULT_CONFIG } from './spinner.config';
 
 @NgModule({
   declarations: [SpinnerComponent],
   imports: [
-    CommonModule
+    CommonModule,
   ]
 })
-export class SpinnerModule { }
+export class NgxUtilitarioSpinnerModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: NgxUtilitarioSpinnerModule,
+      providers: [{provide: SPINNER_INDICATOR_CONFIG, useValue: DEFAULT_CONFIG}]
+    };
+  }
+ }
