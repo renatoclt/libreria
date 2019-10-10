bd5f87f593abc2b0545e54a6ced88ca15dc055fd
renato Cohaila <renatoclt@gmail.com>
2019-10-09 22:50:11 -0500

    eliminar spinner

--- src/app/spinner/spinner.config.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { ISpinnerConfig } from './dto/ispinner-config';
-
-export const DEFAULT_CONFIG: ISpinnerConfig = {
-    size: 160,
-    color: '#17A0DF'
-};
-
-export const SPINNER_INDICATOR_CONFIG: InjectionToken<string> = new InjectionToken('ngx-utilitario-spinner-conf');
